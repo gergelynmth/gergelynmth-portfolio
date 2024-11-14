a9100af0c30e08980d1493a2ad76656d0d9cff53
gergelynmth <gergelynmth@gmail.com>
2024-11-14 19:16:18 +0100

    Add link to a href

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/gergely-nmth/" className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className="uil uil-linkedin-alt"></i>
             </a>
 
-            <a href="" className="home__social-icon" target="_blank">
+            <a href="https://github.com/gergelynmth" className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className="uil uil-github-alt"></i>
-            </a>
-
-            <a href="" className="home__social-icon" target="_blank">
-                <i className="uil uil-instagram"></i>
             </a>
         </div>
     )
